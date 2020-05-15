8202fd6682b9db37d2462c59e50434e9a21ff269
Sandbox Sync <support@getsandbox.com>
2020-05-15 06:24:54 +0000

    Changes

--- Vlocity/main.mjs
+++ Vlocity/main.mjs
@@ -3,8 +3,6 @@
  * 
  */
 
-import { createUserHandler, getUsersHandler, getUserByUsernameHandler } from "users.mjs";
-
 Sandbox.define('/Blacklist', 'GET', function(req, res){
     // send 'Hello world' response
     // res.send('Hello world');
@@ -34,11 +32,3 @@ Sandbox.define('/Blacklist', 'GET', function(req, res){
 }) ;
 
 
-// Using stateful behaviour to simulate creating users
-Sandbox.define('/users', 'POST', createUserHandler);
-
-// Using stateful behaviour to simulate getting all users
-Sandbox.define('/users', 'GET', getUsersHandler);
-
-// Using named route parameters to simulate getting a specific user
-Sandbox.define('/users/{username}', 'GET', getUserByUsernameHandler);
