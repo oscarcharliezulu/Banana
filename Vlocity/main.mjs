c820cf3c3be7c9e4016139edb87cc9890c11dc88
Sandbox Sync <support@getsandbox.com>
2021-06-30 15:30:35 +0000

    Changes

--- Vlocity/main.mjs
+++ Vlocity/main.mjs
@@ -176,34 +176,3 @@ Sandbox.define('/PolicyAdminGetCustomerRecord','GET', function(req, res){
 })
 
 
-//----------------------------------------------------------------------------------
-Sandbox.define('/PolicyAdminGetCustomerRecordXML','GET', function(req, res) {
-    // Check the request, make sure it is a compatible type
-    if (!req.is('application/xml')) {
-        return res.send(400, 'Invalid content type, expected application/xml');
-    }
-    
-    // Set the type of response, sets the content type.
-    res.type('application/xml');
-    // AccId = req.xmlDoc.get("//*[local-name()='AccountId']").text()
-    
-    var xRes = [{
-        "PASAccountNo": AccId,
-        "PASAccountName": "GAIL",
-        "PASStatus": "In Force",
-        "PASBilling": "Current"
-    }];
-    
-    
-    // Set the status code of the response.
-    res.status(200);
-    
-    // Send the response body.
-    res.render('getPolicyAdminGetCustomerRecordXML');
-})
-
-
-
-
-
-
