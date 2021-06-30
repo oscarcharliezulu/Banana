251307491cf163679c7cbd9101b2cb83105617a9
Sandbox Sync <support@getsandbox.com>
2021-06-30 16:01:59 +0000

    Changes

--- Vlocity/main.mjs
+++ Vlocity/main.mjs
@@ -199,3 +199,21 @@ Sandbox.define('/PolicyAdminGetCustomerRecordXML','GET', function(req, res) {
         "status": "ok"
     });
 })
+
+Sandbox.define('/CaseStatus','GET', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
