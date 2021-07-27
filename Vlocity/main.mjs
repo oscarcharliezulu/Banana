9f9d3d5b8fe1c2929e9a538a1ed68518d70ca0f2
Sandbox Sync <support@getsandbox.com>
2021-07-27 07:50:44 +0000

    Changes

--- Vlocity/main.mjs
+++ Vlocity/main.mjs
@@ -247,3 +247,21 @@ Sandbox.define('/RiskRating','GET', function(req, res) {
         "status": "ok"
     });
 })
+
+Sandbox.define('/ms_oauth/oauth2/endpoints/APIserviceprofile/tokens','POST', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
